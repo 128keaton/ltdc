1d6138b0083afcce99d963d2f099029387efacd3
aoineko.fr <aoineko.fr@19bd2cc9-d3be-e927-1f86-49cd67c608c4>
2011-01-17 01:21:39 +0000

    MSXImage tool update

---
+++ MSX/tools/test/color.h
@@ -0,0 +1,28 @@
+#include "types.h"
+
+// color types
+
+struct RGB8;
+struct RGB24;
+
+/** R3 G3 B2 color type */
+struct RGB8
+{
+	u8 RGB;
+
+	RGB8(): RGB(0) {}
+	RGB8(u8 a): RGB(a) {}
+	RGB8(RGB24 color);
+
+	operator u8() { return RGB; }
+};
+
+/** R8 G8 B8 color type */
+struct RGB24
+{
+	u8 R, G, B;
+
+	RGB24(): R(0), G(0), B(0) {}
+	RGB24(u8 a, u8 b, u8 c): R(a), G(b), B(c) {}
+	RGB24(RGB8 color);
+};
