9e7d1bdea84fa2dd53be5f05e8ebeaa2003d6618
aoineko.fr <aoineko.fr@19bd2cc9-d3be-e927-1f86-49cd67c608c4>
2011-01-17 01:21:39 +0000

    MSXImage tool update

---
+++ trunk/MSX/tools/test/MSXImage.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <tchar.h>
+#include "types.h"
+#include "color.h"
+
+//=============================================================================
+// Program
+//=============================================================================
+
+/** Build 256 colors palette */
+void Create256ColorsPalette(const char* filename)
+{
+	RGB24 ColorTable[256];
+	FILE* file;
+	// Create table
+	for(int i=0; i<256; i++)
+	{
+		ColorTable[i] = RGB24(RGB8(i));
+	}
+	// Save
+	fopen_s(&file, filename, "wb");
+	fwrite(ColorTable, sizeof(ColorTable), 1, file);
+	fclose(file);
+}
+
+/** Build 16 colors palette */
+void Create16ColorsPalette(const char* filename)
+{
+	RGB24 ColorTable[256];
+	FILE* file;
+	// Create table
+	for(int i=0; i<256; i++)
+	{
+		switch(i)
+		{
+			case 2:  ColorTable[i] = RGB24(36,  218, 36); break;
+			case 3:  ColorTable[i] = RGB24(109, 255, 109); break;
+			case 4:  ColorTable[i] = RGB24(36,  255, 36); break;
+			case 5:  ColorTable[i] = RGB24(72,  109, 255); break;
+			case 6:  ColorTable[i] = RGB24(182, 36,  36); break;
+			case 7:  ColorTable[i] = RGB24(72,  218, 255); break;
+			case 8:  ColorTable[i] = RGB24(255, 36,  36); break;
+			case 9:  ColorTable[i] = RGB24(255, 109, 109); break;
+			case 10: ColorTable[i] = RGB24(218, 218, 36); break;
+			case 11: ColorTable[i] = RGB24(218, 218, 145); break;
+			case 12: ColorTable[i] = RGB24(36,  145, 36); break;
+			case 13: ColorTable[i] = RGB24(218, 72,  182); break;
+			case 14: ColorTable[i] = RGB24(182, 182, 182); break;
+			case 15: ColorTable[i] = RGB24(255, 255, 255); break;
+			default: ColorTable[i] = RGB24(0, 0, 0);
+		}
+	}
+	// Save
+	fopen_s(&file, filename, "wb");
+	fwrite(ColorTable, sizeof(ColorTable), 1, file);
+	fclose(file);
+}
+
+/** Main entry point */
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Create256ColorsPalette("msx_256.act");
+	Create16ColorsPalette("msx_16.act");
+	return 0;
+}
+
