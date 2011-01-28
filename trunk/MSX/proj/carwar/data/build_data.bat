c74bf53ac8c9d3be2d09e9e14921d36739f4c541
aoineko.fr <aoineko.fr@19bd2cc9-d3be-e927-1f86-49cd67c608c4>
2011-01-28 00:46:24 +0000

    Added support for 3rd and 4th players Added shadow data

---
+++ trunk/MSX/proj/carwar/data/build_data.bat
@@ -0,0 +1,7 @@
+SET
+%MSX_ROOT%\tools\MSXImage\MSXImage.exe -in %MSX_ROOT%\proj\carwar\data\sprt_cars.png -out %MSX_ROOT%\proj\carwar\data\sprt_car_1.h  -pos 0   0  -size 13 11 -num 16 1 -trans 0xE300E3 -name car1
+%MSX_ROOT%\tools\MSXImage\MSXImage.exe -in %MSX_ROOT%\proj\carwar\data\sprt_cars.png -out %MSX_ROOT%\proj\carwar\data\sprt_car_2.h  -pos 0   11 -size 13 11 -num 16 1 -trans 0xE300E3 -name car2
+%MSX_ROOT%\tools\MSXImage\MSXImage.exe -in %MSX_ROOT%\proj\carwar\data\sprt_cars.png -out %MSX_ROOT%\proj\carwar\data\sprt_car_3.h  -pos 0   22 -size 13 11 -num 16 1 -trans 0xE300E3 -name car3
+%MSX_ROOT%\tools\MSXImage\MSXImage.exe -in %MSX_ROOT%\proj\carwar\data\sprt_cars.png -out %MSX_ROOT%\proj\carwar\data\sprt_car_4.h  -pos 0   33 -size 13 11 -num 16 1 -trans 0xE300E3 -name car4
+%MSX_ROOT%\tools\MSXImage\MSXImage.exe -in %MSX_ROOT%\proj\carwar\data\sprt_cars.png -out %MSX_ROOT%\proj\carwar\data\sprt_shadow.h -pos 208 0  -size 13 11 -num 1  1 -trans 0xE300E3 -name shadow
+pause
